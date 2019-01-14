b42af684a5d85b44ed4b01098a33af8794bf818c
Lars Nihlmark <larsnihlmark@gmail.com>
2019-01-14 20:45:14 +0100

    connected my buttons with js and to php

---
+++ java.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+
+    viewHoroscope = function(){
+        $.ajax({
+            url:"viewHoroscope.php",
+            method: "GET",
+            success: function(results){
+                $(".content").html(results);
+            }
+        })
+    }
+
+
+/* viewHoroscope(); */
+$("#visaHoroscope").click(function(){
+    viewHoroscope();
+});
+
+$("#sparaHoroscope").click(function(){
+
+    $.ajax({
+        url:"addHoroscope.php",
+        method:"POST",
+        data:{
+            "personNr": $("#angivetnummer").val()
+        },
+        success: function(results){
+            if(results == true){
+                viewHoroscope();
+            }
+            else {
+                $(".content").html(results);
+            }
+        }
+    });
+});
+
+    $("#uppdateraHoroscope").click(function(){
+        $.ajax({
+            url:"updateHoroscope.php",
+            method: "PUT",
+            data:{
+                "personNr": $("#angivetNummer").val()
+            },
+            success: function(results){
+                if(results == true){
+                    viewHoroscope();
+                }   else  {
+                    $(".content").html(results);
+                }
+            }
+        }); 
+    });
+
+    $("#raderaHoroscope").click(function(){
+                
+        $.ajax({
+            url:"php/deleteHoroscope.php",
+            method: "DELETE",
+            data:{
+                "personNr": $("#angivetNummer").val()
+            },
+            success: function(results){
+                $(".content").html(results);
+            }
+        });   
+    });
+});
