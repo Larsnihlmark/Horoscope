13a021818ccb8ccc6d50453e98979851efb9b929
Lars Nihlmark <larsnihlmark@gmail.com>
2019-01-17 01:23:30 +0100

    change text

--- viewHoroscope.php
+++ viewHoroscope.php
@@ -3,11 +3,12 @@
 session_start();
 if($_SERVER["REQUEST_METHOD"] == "GET"){
 
-    if($_SESSION == null){
-        echo "<p>Det finns inget horoskop sparat</p>";
+    if($_SESSION == !null){
+        echo ($_SESSION["horoscope"]);
+        
     }
     else{
-        echo ($_SESSION["horoscope"]);
+        echo "<p>Det finns inget horoskop sparat!</p>";
     }
 }
 ?>
